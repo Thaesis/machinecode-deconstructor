7d1ca5db05e975e1c7fe2a878b163278505dea5b
Thaesis <142756409+Thaesis@users.noreply.github.com>
2023-10-07 21:07:58 +0000

    Addition of Structs

--- main.go
+++ main.go
@@ -57,7 +57,21 @@ func main() {
 	}
 
 	inFile.Close()
-	outFile.Close()
+
+  // Open output file for writing
+	outFile, outErr = os.Create(*outputFileName)
+	if outErr != nil {
+		log.Fatalf("Error creating output file: %s", outErr)
+	}
+	defer outFile.Close()
+
+  pc := 96 //initial program counter value
+  for _, line := range lnData {
+		instruction := parseMachineCode(line)
+		disassembled := disassembleInstruction(instruction, pc)
+		fmt.Fprintf(outFile, "%s %s\n", line, disassembled)
+		pc += 4 // Assuming each instruction is 4 bytes
+	}
 
 }
 
@@ -74,5 +88,45 @@ func parseMachineCode(machineCode string) ARMInstruction {
 		DestReg: int(destReg),
 		SrcReg1: int(srcReg1),
 		SrcReg2: int(srcReg2),
+	}
+}
+
+func disassembleInstruction(instr ARMInstruction, pc int) string {
+	switch instr.Opcode {
+	case "100010": // ADD
+		return fmt.Sprintf("ADD R%d, R%d, R%d", instr.DestReg, instr.SrcReg1, instr.SrcReg2)
+	case "110010": // SUB
+		return fmt.Sprintf("SUB R%d, R%d, R%d", instr.DestReg, instr.SrcReg1, instr.SrcReg2)
+	case "100000": // AND
+		return fmt.Sprintf("AND R%d, R%d, R%d", instr.DestReg, instr.SrcReg1, instr.SrcReg2)
+	case "101000": // ORR
+		return fmt.Sprintf("ORR R%d, R%d, R%d", instr.DestReg, instr.SrcReg1, instr.SrcReg2)
+	case "000101": // B
+		offset, _ := strconv.ParseInt(instr.Opcode[6:], 2, 32)
+		// Adjust for signed value
+		offset <<= 2
+		target := pc + int(offset)
+		return fmt.Sprintf("B #%d", target)
+	case "100100": // ADDI
+		imm, _ := strconv.ParseInt(instr.Opcode[6:], 2, 32)
+		return fmt.Sprintf("ADDI R%d, R%d, #%d", instr.DestReg, instr.SrcReg1, imm)
+	case "110100": // SUBI
+		imm, _ := strconv.ParseInt(instr.Opcode[6:], 2, 32)
+		return fmt.Sprintf("SUBI R%d, R%d, #%d", instr.DestReg, instr.SrcReg1, imm)
+	case "111110": // LDUR, STUR
+		offset, _ := strconv.ParseInt(instr.Opcode[6:], 2, 32)
+		return fmt.Sprintf("%s R%d, [R%d, #%d]", "LDUR", instr.DestReg, instr.SrcReg1, offset)
+	case "101101": // CBZ, CBNZ
+		offset, _ := strconv.ParseInt(instr.Opcode[6:], 2, 32)
+		// Adjust for signed value
+		offset <<= 2
+		target := pc + int(offset)
+		if instr.DestReg&1 == 0 {
+			return fmt.Sprintf("CBZ R%d, #%d", instr.DestReg, target)
+		} else {
+			return fmt.Sprintf("CBNZ R%d, #%d", instr.DestReg, target)
+		}
+	default:
+		return "UNKNOWN INSTRUCTION"
 	}
 }
